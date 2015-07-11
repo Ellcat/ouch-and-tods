2be2a117eb3097b02609310d74f3acad512e1074
ChristineEllis <jadeapple@gmail.com>
2015-07-11 15:56:23 +0000

    Configured quote controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Splurty::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+ 
+ root 'quotes#index' 
+
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
